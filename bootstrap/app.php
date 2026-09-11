65b3a996be95b5899fef25930d69f214a5aca038
pooj25 <poojagovindharaj75@gmail.com>
2026-09-11 08:52:59 +0530

    Force HTTPS: add URL::forceScheme in production and trust all proxies for Render reverse proxy - fixes 'not secure' warning on form submit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
